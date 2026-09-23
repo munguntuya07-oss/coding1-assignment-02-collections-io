f088a85ee8a793f126272277dd06053bcfe3e4b3
Munguntuya Zolbayar <zolbayar_munguntuya@student.ceu.edu>
2026-09-23 17:24:27 +0200

    assignment done

--- assignment.py
+++ assignment.py
@@ -1,6 +1,13 @@
+# storing drink name
 drink = "tea"
+# storing sales number of tea is 3 and number of coffee is 5
 sales = {"tea": 3, "coffee": 5}
+# getting number of sold tea
 sold = sales[drink]
+# increase the number of teas sales by 1
 sold = sold + 1
+# print drink name
 print(drink)
+# print the updated/last number of tea's sales
 print(sold)
+# prediction the 2 prints will drink name and updated sales number
